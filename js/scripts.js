c85e62e3c0ecbb7fed59d1d9c1135c8485a44b0d
Andrew Kim <andrewswkim47@gmail.com>
2020-02-05 19:56:07 -0800

    add more logic to scripts.js, add variables equal to an array, and variables for different expected outcomes, and start to add function with for loop.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,18 @@ $(document).ready(function(){
   $("form").submit(function(){
     // alert("this is working!")
 // var input1 = parseInt($("#num1").val());
-var input1 = [];
-for 
+var inputOne = [];
+var one = "Beep!";
+var two = "Boop!";
+var three = "I'm sorry, Dave. I'm afraid I can't do that."
 
+function numberRange() {
+for (var index=0; index <= input1; index++) {
+  // alert("this is working");
+  inputOne.toString();
+}
+}
+numberRange();
     event.preventDefault();
   });
 });
