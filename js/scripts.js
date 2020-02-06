d1e12d7bce487f0edb09512289f919824dcf3dac
Andrew Kim <andrewswkim47@gmail.com>
2020-02-05 19:39:14 -0800

    show more bootstrap styling specifically on input field and button, as well as whole container. Started to work on jquery logic.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("form").submit(function(){
+    // alert("this is working!")
+// var input1 = parseInt($("#num1").val());
+var input1 = [];
+for 
+
+    event.preventDefault();
+  });
+});
