962a630948c01351daf8c6cb05beec4831106d5c
Andrew Kim <andrew.kim@thesis.agency>
2020-02-06 19:05:45 -0800

    get an array of values but not all are returned correctly.

--- js/scripts3.js
+++ js/scripts3.js
@@ -6,15 +6,15 @@ function numberRange(input) {
   for (var i=0; i <= input; i++) {
     // alert("this is working");
     var inputString = input.toString();
+
     // alert("userNumber");
-    if (inputString !=0 && i % 3 === 0) {
+    if (i % 3 === 0 && i !== 0) {
         inputRange.push("yo");
-       } else if (inputString === 2) {
+       } else if (inputString.includes("1")) {
          inputRange.push("Boop!");
-       } else if (inputString === 1) {
+       } else if (inputString.includes("2")) {
         inputRange.push("Beep!");
        } else {
-         (inputString === 0)
          inputRange.push(inputString);
        }
     } 
