40aa4ac76d133ed090cb76da998979b1956b7c42
Andrew Kim <andrewswkim47@gmail.com>
2020-02-07 16:32:45 -0800

    remove other scripts files not being used.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,31 @@
-//Business Logic
 function numberRange(input) {
   var inputRange = [];
-  // var one = "Beep!";
-  // var two = "Boop!";
-  // var three = "I'm sorry, Dave. I'm afraid I can't do that."
-    for (var i=0; i <= input; i++) {
-      // alert("this is working");
-      var inputString = input.toString();
-      // alert("userNumber");
-      if (inputString.includes(1)) {
-        // console.log("yes");
-        inputRange.push("Beep!");
-        console.log(inputRange);
-         } else if (inputString.includes(2)) {
-         // console.log("yes");
-           inputRange.push("Boop!");
-         } else if (inputString.includes(3)) {
-           // console.log("yes");
-           inputRange.push("I'm sorry, Dave. I'm afraid I can't do that.");
-         } else {
-           // console.log("yes");
-          inputRange.push(input.toString());
-          return input
-          }
-      // console.log(input);
-      numberRange(3);
+
+  for (var i=0; i <= input; i++) {
+    if (i.toString().includes("3")) {
+        inputRange.push(" I'm sorry, Dave. I'm afraid I can't do that.");
+       } else if (i.toString().includes("2")) {
+         inputRange.push(" Boop!");
+       } else if (i.toString().includes("1")) {
+        inputRange.push(" Beep!");
+       } else {
+         inputRange.push(i);
+       }
     } 
-   
-}
-   
+    return inputRange
+  }
 
-//start user interface logic
-// $(document).ready(function(){
-//   $("form").submit(function(event){
-//     event.preventDefault();
 
-//     var inputResult = $("#numOne").val();
-//     // alert(inputResult);
-//     $('body').append(`<div class="outputMargin"><p>${inputResult}</p></div>`);
-//   });
-// });
+// start user interface logic
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    var input = parseInt($("#numOne").val());
+    var inputFive = 5
+    var inputThree = 3
+
+    $('body').append(`<div class="outputMargin text-white font-weight-bold"><p>${numberRange(input)}</p></div>`);
+    $('body').append(`<div class="outputMargin text-white font-weight-bold"><p>${"The Jackson Five"}</p><img src="img/jacksonfive.jpg" alt="The Jackson Five"/></div>`);
+    $('body').append(`<div class="outputMargin text-white font-weight-bold"><p>${"The Answer"}</p><img src="img/ai.jpg" alt="Allen Iverson"/></div>`);
+  });
+});
