0c22e0a6593fa8e588c1c1a27883698522be422d
Andrew Kim <andrewswkim47@gmail.com>
2020-02-07 18:49:51 -0800

    add boostrap css to new reveal button under kobe image.

--- js/scripts.js
+++ js/scripts.js
@@ -32,16 +32,14 @@ $(document).ready(function(){
     $("#resultText").append(`<div class="outputMargin text-white font-weight-bold"><p>${numberRange(input)}</p></div>`);
     if (input === 8 || input === 24) {
       $("#mamba").show();
-      $("#jackson").show()
+      $("#reveal").show()
   }
-   
 
-    
-
-
+    $("#reveal").click(function(){
+      $("#flip").slideDown("slow");
+    });
     // $('body').append(`<div class="outputMargin text-white font-weight-bold"><p>${numberRange(input)}</p></div>`);
     // $('body').append(`<div class="outputMargin text-white font-weight-bold"><p>${"The Jackson Five"}</p><img src="img/jacksonfive.jpg" alt="The Jackson Five"/></div>`);
     // $('body').append(`<div class="outputMargin text-white font-weight-bold"><p>${"The Answer"}</p><img src="img/ai.jpg" alt="Allen Iverson"/></div>`);
-   
   });
 });
